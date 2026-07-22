18bb1a8235744d5eecd35ad89c8623c900a041f2
abhikamyams <abhikamya.ms@students.iiserpune.ac.in>
2026-07-22 19:11:01 +0530

    Update make_loss_plots.py

--- make_loss_plots.py
+++ make_loss_plots.py
@@ -1,55 +1,82 @@
-import numpy as np 
 import matplotlib.pyplot as plt
-results = np.load('results.npy')
+import dask.dataframe as dd
+import numpy as np
 
-print(np.shape(results))
-w_dist=results[0,:]
-kl=results[2,:]
-epochs=[i for i in range (len(w_dist))]
-gradient_penalty=results[3,:]
-plt.plot(epochs,w_dist,label='w_dist')
-plt.plot(epochs,gradient_penalty,label='gp')
-# plt.xlabel('x in cms', fontsize=11)
-plt.xlabel('epochs', fontsize=11)
-# plt.yscale('log')
-plt.title('w distance and gradient penalty')
-plt.subplots_adjust(left=0.1, bottom=0.15, right=0.9, top=0.85)
-plt.grid(visible=True)
-plt.legend()
-plt.savefig('w_dist_and_gp.png')
+#load FastSim and FullSim data
+FastSim=np.load('/home/ubuntu/Abhikamya/Final/FastSim_phonons_total.npy',allow_pickle=True)
+FullSim=dd.read_parquet('/home/ubuntu/Abhikamya/Original_root_files/Output1.parquet', 
+columns=["Final_positionX", "Final_positionY","Deposited_energy", "Final_time"])
+
+#take the same number of phonons from both data sets
+FullSim=np.array(FullSim.head(FastSim.shape[0],npartitions=2))
+
+#plot FullSim 1D histograms
+plt.figure(figsize=(12,12))
+plt.subplot(2,2,1,title='Final Position X',xlabel='X in m', ylabel='Counts in each bin')
+plt.hist(FullSim[:,0],bins=100)
+
+plt.subplot(2,2,2,title='Final Position Y',xlabel='Y in m', ylabel='Counts in each bin')
+plt.hist(FullSim[:,1],bins=100)
+
+plt.subplot(2,2,3,title='Deposited Energy',xlabel='Energy in eV', ylabel='Counts in each bin')
+plt.hist(FullSim[:,2],bins=100)
+plt.xscale('log')
+
+plt.subplot(2,2,4,title='Final Time',xlabel='Time ns ', ylabel='Counts in each bin')
+plt.hist(FullSim[:,3],bins=100)
+plt.tight_layout()
+plt.savefig('FullSim_1D.png')
 plt.close('all')
 
-# plt.plot(epochs,w_dist,label='w_dist')
-# plt.plot(epochs,gradient_penalty,label='gp')
-# plt.grid(visible=True)
-# plt.subplots_adjust(left=0.15, bottom=0.15, right=0.82, top=0.85)
-# plt.legend()
-# plt.savefig('w_dist_and_gp.png')
-# plt.close('all')
 
-plt.plot(epochs,kl,label='kl divergence')
-plt.grid(visible=True)
-plt.xlabel('epochs', fontsize=11)
-plt.ylabel('kl divergence')
+#plot FastSim 1D histograms
+plt.figure(figsize=(12,12))
+plt.subplot(2,2,1,title='Final Position X',xlabel='X in m', ylabel='Counts in each bin')
+plt.hist(FastSim[:,0],bins=100)
+
+plt.subplot(2,2,2,title='Final Position Y',xlabel='Y in m', ylabel='Counts in each bin')
+plt.hist(FastSim[:,1],bins=100)
+
+plt.subplot(2,2,3,title='Deposited Energy',xlabel='Energy in eV', ylabel='Counts in each bin')
+plt.hist(FastSim[:,2],bins=100)
+plt.xscale('log')
+
+plt.subplot(2,2,4,title='Final Time',xlabel='Time ns ', ylabel='Counts in each bin')
+plt.hist(FastSim[:,3],bins=100)
+plt.tight_layout()
+plt.savefig('FastSim_1D.png')
+plt.close('all')
+
+
+#plot comparison histograms 
+plt.figure(figsize=(12,12))
+plt.subplot(2,2,1,title='Final Position X',xlabel='X in m', ylabel='Counts in each bin')
+plt.hist(FullSim[:,0],bins=100,alpha=0.5,label='FullSim')
+plt.hist(FastSim[:,0],bins=100,alpha=0.5,label='FastSim')
+
+plt.subplot(2,2,2,title='Final Position Y',xlabel='Y in m', ylabel='Counts in each bin')
+plt.hist(FullSim[:,1],bins=100,alpha=0.5,label='FullSim')
+plt.hist(FastSim[:,1],bins=100,alpha=0.5,label='FastSim')
+
+plt.subplot(2,2,3,title='Deposited Energy',xlabel='Energy in eV', ylabel='Counts in each bin')
+plt.hist(FullSim[:,2],bins=100,alpha=0.5,label='FullSim')
+plt.hist(FastSim[:,2],bins=100,alpha=0.5,label='FastSim')
+plt.xscale('log')
+
+plt.subplot(2,2,4,title='Final Time',xlabel='Time ns ', ylabel='Counts in each bin')
+plt.hist(FullSim[:,3],bins=100,alpha=0.5,label='FullSim')
+plt.hist(FastSim[:,3],bins=100,alpha=0.5,label='FastSim')
+plt.tight_layout()
+plt.legend()
+plt.savefig('Comparision_histograms.png')
+plt.close('all')
+
+
+#energy comparision zoomed in on the discrete range
+plt.plot(title='Deposited Energy',xlabel='Energy in eV', ylabel='Counts in each bin')
+plt.hist(np.log(FullSim[:,2]),bins=1000,alpha=0.5,label='FullSim',range=(-22,-8))
+plt.hist(np.log(FastSim[:,2]),bins=1000,alpha=0.5,label='FastSim',range=(-22,-8))
+
 plt.yscale('log')
-plt.subplots_adjust(left=0.2, bottom=0.15, right=0.8, top=0.85)
 plt.legend()
-plt.savefig('Kl_divergence.png')
-plt.close('all')
-
-plt.plot(epochs,-w_dist+gradient_penalty,label='d_loss')
-plt.xlabel('epochs', fontsize=11)
-plt.plot(epochs,-w_dist,label='-wdist')
-plt.plot(epochs,gradient_penalty,label='gradient penalty')
-plt.subplots_adjust(left=0.1, bottom=0.15, right=0.9, top=0.85)
-plt.grid(visible=True)
-
-plt.legend()
-plt.savefig('d_loss.png')
-
-# plt.title(f'Histograms for caustics 1/10, after epoch {epoch}',pad=15)
-# plt.xlabel('x in cms', fontsize=11)
-# plt.ylabel('y in cms', fontsize=11)
-#     cbar = plt.colorbar()
-#     cbar.set_label('Counts in each pixel', labelpad=15, fontsize=11)
-#     plt.subplots_adjust(left=0.15, bottom=0.15, right=0.82, top=0.85)
+plt.savefig('energy_comparison.png')
